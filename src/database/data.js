da307be8a9c9cb0accbc494932758c20fd16beda
Kalpesh Badgujar <89139455+BadgujarKalpesh@users.noreply.github.com>
2023-12-22 22:36:48 +0530

    Add files via upload

---
+++ src/database/data.js
@@ -0,0 +1,184 @@
+export default [
+    {
+        id: 1,
+        question: "Identify the type of the underlined word in the sentence: 'She quickly ran to the store.' The underlined word 'quickly' is a/an ________.",
+        options: [
+            'Adverb',
+            'Verb',
+            'Adjective',
+        ]
+    },
+    {
+        id: 2,
+        question: "Which of these is a correct sentence?",
+        options: [
+            'He don’t like tomatoes.',
+            'She haven’t been there before.',
+            'They have gone to the beach.'
+        ]
+    },
+    {
+        id: 3,
+        question: "Select the correct usage: 'Their' or 'There'?",
+        options: [
+            'Their going to the park.',
+            'He lives over there.',
+            'Their are many books on the shelf.'
+        ]
+    },
+    {
+        id: 4,
+        question: "Identify the part of speech of the word 'happily' in the sentence: 'She sang happily all day.' The word 'happily' functions as a/an ________.",
+        options: [
+            'Adverb',
+            'Adjective',
+            'Noun',
+        ]
+    },
+    {
+        id: 5,
+        question: "Which sentence uses correct punctuation?",
+        options: [
+            'The cat is mine.',
+            'The cat its mine.',
+            'The cat, its mine.'
+        ]
+    },
+    {
+        id: 6,
+        question: "Choose the correct form of the verb: 'Yesterday, she ________ to the market.'",
+        options: [
+            'go',
+            'goes',
+            'gone',
+        ]
+    },
+    {
+        id: 7,
+        question: "What is the plural form of 'child'?",
+        options: [
+            'Childs',
+            'Childrens',
+            'Children',
+        ]
+    },
+    {
+        id: 8,
+        question: "Which of these is a conjunction?",
+        options: [
+            'And',
+            'Run',
+            'Quickly',
+        ]
+    },
+    {
+        id: 9,
+        question: "What is the past tense of the verb 'to teach'?",
+        options: [
+            'Teached',
+            'Taught',
+            'Teach',
+        ]
+    },
+    {
+        id: 10,
+        question: "Select the correct sentence: 'I will go to the party if ________.'",
+        options: [
+            'He come with me.',
+            'He comes with me.',
+            'He came with me.',
+        ]
+    },
+    {
+        id: 11,
+        question: "Choose the correct possessive pronoun: 'That book belongs to ________.'",
+        options: [
+            'Her',
+            'She',
+            'Hers',
+        ]
+    },
+    {
+        id: 12,
+        question: "What is the comparative form of 'good'?",
+        options: [
+            'Better',
+            'Gooder',
+            'Best',
+        ]
+    },
+    {
+        id: 13,
+        question: "Which sentence is in the passive voice?",
+        options: [
+            'She sang a song.',
+            'A song was sung by her.',
+            'She is singing a song.',
+        ]
+    },
+    {
+        id: 14,
+        question: "Choose the correct form of the adjective: 'The weather is ________ today.'",
+        options: [
+            'Good',
+            'Well',
+            'Goodly',
+        ]
+    },
+    {
+        id: 15,
+        question: "What is the superlative form of 'far'?",
+        options: [
+            'Farther',
+            'More far',
+            'Farthest',
+        ]
+    },
+    {
+        id: 16,
+        question: "Which word is a preposition in the sentence: 'The cat is on the table.'",
+        options: [
+            'Cat',
+            'Is',
+            'On',
+        ]
+    },
+    {
+        id: 17,
+        question: "Select the correct use of the apostrophe: 'The books ________ covers are torn.'",
+        options: [
+            'Book’s',
+            'Books’',
+            'Books',
+        ]
+    },
+    {
+        id: 18,
+        question: "Identify the type of sentence: 'Where are you going?'",
+        options: [
+            'Interrogative',
+            'Exclamatory',
+            'Declarative',
+        ]
+    },
+    {
+        id: 19,
+        question: "Choose the correct spelling:",
+        options: [
+            'Accomodate',
+            'Acommodate',
+            'Accommodate',
+        ]
+    },
+    {
+        id: 20,
+        question: "What is the antonym of 'brave'?",
+        options: [
+            'Fearless',
+            'Cowardly',
+            'Courageous',
+        ]
+    }
+];
+
+export const answers = [0, 2, 1, 0, 2, 1, 2, 0, 1, 1, 2, 0, 1, 0, 2, 2, 1, 0, 2, 1];
